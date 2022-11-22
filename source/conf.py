1185cd1dcc8b6b40c6e4edc7133c3d751a1ece92
liuzheng <981638732@qq.com>
2022-11-22 17:47:16 +0800

    docs

---
+++ source/conf.py
@@ -0,0 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Demo'
+copyright = '2022, liuzheng'
+author = 'liuzheng'
+release = 'v1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['recommonmark','sphinx_markdown_tables']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'zh_CN'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+import sphinx_bernard_theme
+html_theme = 'sphinx_bernard_theme'
+html_theme_path = [sphinx_bernard_theme.get_html_theme_path()]
+
+html_static_path = ['_static']
---
+++ source/index.rst
@@ -0,0 +1,13 @@
+.. Demo documentation master file, created by
+   sphinx-quickstart on Tue Nov 22 17:02:21 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Demo's documentation!
+================================
+
+.. toctree::
+   :maxdepth: 3
+   :caption: Contents:
+   
+   Cpp/index
